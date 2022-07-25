9dc1648bcd7567a89b3808d42d7a258c02c20904
jrmeyers92 <JakeRMeyers92@gmail.com>
2022-07-24 19:55:34 -0500

    pages and routing

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function AboutPage() {
+  return (
+    <div>
+        <h1>About</h1>
+        <p>This is an app to find local events near you.</p>
+        <p>Version 1.0.0</p>
+    </div>
+  )
+}
