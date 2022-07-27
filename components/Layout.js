152dc2057f51494f23e07af388330a1343bdd8aa
jrmeyers92 <JakeRMeyers92@gmail.com>
2022-07-26 20:46:30 -0500

    add images and create showcase

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,14 @@
 import Head from "next/head"
+import { useRouter } from "next/router"
 import styles from "@/styles/Layout.module.css"
 import Header from "./Header"
 import Footer from "./Footer"
+import Showcase from "./Showcase"
 
 
 export default function Layout({title, description, keywords, children}) {
+  const router = useRouter();
+
   return (
     <div>
         <Head>
@@ -13,6 +17,9 @@ export default function Layout({title, description, keywords, children}) {
             <meta name="keywords" content={keywords} />
         </Head>
         <Header />
+
+        { router.pathname === "/" && <Showcase /> }
+        
         <div className={styles.container}>
           {children}
         </div>
---
+++ components/Showcase.js
@@ -0,0 +1,11 @@
+import styles from  "@/styles/Showcase.module.css"
+
+export default function Showcase() {
+  return (
+    <div className={styles.showcase}>
+        <h1>Welcome To Events Central!</h1>
+        <h2>Find The Best Events Near You</h2>
+        
+    </div>
+  )
+}
